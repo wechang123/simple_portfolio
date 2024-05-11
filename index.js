4290480eb2f7dd2eb9182ddde70169e36f5c0d71
wichang <wechang123@naver.com>
2024-05-11 19:07:23 +0900

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let moveToTop = function () {
 
 // 타이핑 효과
 const $txt = document.querySelector(".txt-title");
-const content = "안녕하세요 :)\n개발꿈나무 주혜린입니다.";
+const content = "안녕하세요 :)\n개발꿈나무 유위창입니다.";
 let contentIndex = 0;
 
 let typing = function () {
